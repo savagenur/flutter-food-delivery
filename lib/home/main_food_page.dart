fa0f6d8bfa96e668d3dc859520e287b83a739478
savagenur <savagenurkg@gmail.com>
2022-04-03 02:44:27 +0600

    Pageview, Matrix4, Transform, Dots indicator

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
@@ -17,6 +18,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
     return Scaffold(
       body: Column(
         children: [
+          // Showing the header
           Container(
             child: Container(
               margin: const EdgeInsets.only(top: 45, bottom: 15),
@@ -26,10 +28,16 @@ class _MainFoodPageState extends State<MainFoodPage> {
                 children: [
                   Column(
                     children: [
-                      BigText(text: 'Bangladesh', color: AppColors.mainColor,),
+                      BigText(
+                        text: 'Bangladesh',
+                        color: AppColors.mainColor,
+                      ),
                       Row(
                         children: [
-                          SmallText(text: 'Narsingdi', color: Colors.black54,),
+                          SmallText(
+                            text: 'Narsingdi',
+                            color: Colors.black54,
+                          ),
                           Icon(Icons.arrow_drop_down_rounded),
                         ],
                       ),
@@ -43,13 +51,18 @@ class _MainFoodPageState extends State<MainFoodPage> {
                         borderRadius: BorderRadius.circular(15),
                         color: Colors.blue,
                       ),
-                      child: Icon(Icons.search, color: Colors.white,),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
                     ),
                   )
                 ],
               ),
             ),
-          )
+          ),
+          // Showing the body
+          FoodPageBody(),
         ],
       ),
     );
